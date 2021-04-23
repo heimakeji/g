54d4f8e75de3d5f2452e8f9cc6ad39ae5b87aa17
heimakeji <63442677+heimakeji@users.noreply.github.com>
2021-04-24 00:37:55 +0800

    Add files via upload

---
+++ jsGetCodexb.js
@@ -0,0 +1,15 @@
+OnGetCode = function ( nType )
+{
+    let strCode = "";
+    //public
+    if( nType == 0 )
+    {
+        strCode = "+7i/RXXjtQn9cbMAvBj4BQ5zndzuuyC+bot6Kguk5kTlseqpLmYnmOqGiIKOEL7NsEj1PmQf+1fEhAZ0m/xhBcwLMi+azoopKrYXLrp4NPKD5ZV1h+Agv7yfklqMh3UmHQxR4c/5en0yehCuD10iIw==";
+    }
+    //Dev
+    else
+    {
+        strCode = "+7i/RXXjtQn9cbMAvBj4BQ5zndzuuyC+bot6Kguk5kTlseqpLmYnmOqGiIKOEL7NsEj1PmQf+1fEhAZ0m/xhBcwLMi+azoopKrYXLrp4NPKD5ZV1h+Agv7yfklqMh3UmStx6qkPRpWUHlsScSPPxxjtocaxHpAK8jHFO1O9+uu0=";
+    }
+    return  strCode;
+}
